fb725f8f751e9385fa17e949642b5d0e4da0cbe4
Mark Koelle <mkoelle@gmail.com>
2020-06-14 01:41:46 -0500

    Add inital data files for site

---
+++ src/_data/carrer.js
@@ -0,0 +1,92 @@
+module.exports = {
+  summary: ``,
+  positions: [
+    {
+      company: "VWCredit",
+      location: "Libertyville, IL.",
+      title: "Senior DevOps Engineer",
+      period: "JUN 2019 - PRESENT",
+      jobdesc: [],
+    },
+    {
+      company: "Volkswagen Financials",
+      location: "Berlin, Germany",
+      title: "Full Stack Web Developer",
+      period: "2018 - 2019",
+      jobdesc: [],
+    },
+    {
+      company: "VWCredit",
+      location: "Libertyville, IL.",
+      title: "DevOps Engineer",
+      period: "2008 - 2018",
+      jobdesc: [],
+    },
+    {
+      company: "VWCredit",
+      location: "Libertyville, IL.",
+      title: "Software Developer",
+      period: "2008 - 2018",
+      jobdesc: [],
+    },
+  ],
+  education: [
+    {
+      university: "Indiana University",
+      city: "Bloomington, In",
+      degree: "Bachelor of Science in Informatics",
+      period: "2008",
+      faculty: "Dual Minors in Business and in Information Systems",
+    },
+  ],
+  projects: [
+    {
+      title: "Bifrost",
+      company: "Volkswagen Financials",
+      period: "2018 - 2020",
+      technologies: ["AWS","Python", "Typescript","Javascript","Cloudformation","Vue", "Lambda", "Concourse CI","KeyCloak","GIT"]
+    },
+    {
+      title: "Cloverleaf",
+      company: "Volkswagen Credit",
+      period: "2016 - 2018",
+      technologies: [".net 4.6", "EF6", "SpecFlow","WCF","ASP.Net MVC 5","Oracle","SQL Server","TDD", "BDD","NuGet", "TFS"]
+    },
+    {
+      title: "IVR Composer Rewrite",
+      company: "Volkswagen Credit",
+      period: "2016 - 2018",
+      technologies: [".NET 4.5", "EF5", "SpecFlow", "WCF", "ASP.NET MVC 4", "Oracle", "TDD", "BDD", "NuGet", "Composer", "Java", "Splunk", "Metro"]
+    },
+    {
+      title: "Enterprise Shared Service",
+      company: "Volkswagen Credit",
+      period: "2016 - 2018",
+      technologies: [".NET 4.5", "EF5", "SpecFlow", "WCF", "ASP.NET MVC 4", "Oracle", "TDD", "BDD", "NuGet", "Microsoft Release Manager"]
+    },
+    {
+      title: "Cell Phone Scrubbing",
+      company: "Volkswagen Credit",
+      period: "2016 - 2018",
+      technologies: [".NET 4.5", "EF5", "SpecFlow", "WCF", "ASP.NET MVC 4", "Oracle", "TDD", "BDD", "NuGet"]
+    },
+  ],
+  skills: [
+    {
+      name: "Agile",
+      progress: 85,
+    },
+    {
+      name: "TDD",
+      progress: 80,
+    },
+    {
+      name: "Backend Development",
+      progress: 90,
+    },
+    {
+      name: "CSS",
+      progress: 30,
+    },
+  ],
+};
---
+++ src/_data/profile.js
@@ -0,0 +1,24 @@
+module.exports = {
+  my: {
+    name: "Mark Koelle",
+    title: "Senior DevOps Engineer",
+    city: "Lindenhurst, Il",
+    phone: "(+1) 847-250-7337",
+    mail: "mkoelle@gmail.com",
+  },
+  social: {
+    twitter: {
+      name: "@mkoelle",
+      link: "https://twitter.com/mkoelle",
+    },
+    linkedin: {
+      name: "in/mkoelle",
+      link: "https://www.linkedin.com/in/mkoelle",
+    },
+    github: {
+      name: "mkoelle",
+      link: "https://github.com/mkoelle",
+    },
+  },
+  languages: ["English (Native)", "German (A2)"],
+}
